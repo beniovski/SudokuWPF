65d0e06e97d186f455df21cafd493187b7f3d764
DESKTOP-8BK3PKQ\Daniel <danbed1990@gmail.com>
2019-08-28 20:35:42 +0200

    Create View for game

--- Sudoku/MainWindow.xaml.cs
+++ Sudoku/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,58 @@ namespace Sudoku
         public MainWindow()
         {
             InitializeComponent();
+            TextBoxGeneration();
         }
+
+        private void TextBoxGeneration()
+        {
+            int textBoxNum = 81;
+            int colNum = 1;
+            int pozInRow = 1;
+
+            for(int i = 1; i <= textBoxNum; i++)
+            {
+                var el = new TextBox();
+                el.Height = 30;
+                el.Width = 30;
+                string name = colNum.ToString() + ":" + pozInRow.ToString();
+                el.Name = "box"+i;
+                el.TextAlignment = TextAlignment.Center;
+                el.VerticalAlignment = VerticalAlignment.Center;
+                el.FontSize = 20;
+                el.MaxLength = 1;
+                el.PreviewTextInput += InputValidation;
+                el.Text = "1";
+                el.IsReadOnly = true;
+                el.Background = Brushes.LightSlateGray;
+                
+                if (i%9 == 0)
+                {
+                    Canvas.SetLeft(el, 45 * pozInRow);
+                    Canvas.SetTop(el, 45 * colNum);
+                    canvas1.Children.Add(el);
+                    colNum++;
+                    pozInRow = 1;
+                }
+                else
+                {
+                    Canvas.SetLeft(el, 45 * pozInRow);
+                    Canvas.SetTop(el, 45 * colNum);
+                    canvas1.Children.Add(el);
+                    pozInRow++;
+                }             
+
+                
+               
+            }
+        }
+
+        private void InputValidation(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+       
     }
 }
